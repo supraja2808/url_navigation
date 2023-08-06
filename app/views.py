4970a1fb417be0e8fa122ae4c2eb107543918664
supraja <supraja2808@gmail.com>
2023-08-06 12:49:19 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def suppu(request):
+    return render(request,'suppu.html')
---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def prathibha(request):
+    return render(request,'prathibha.html')
